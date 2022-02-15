21be3060e4fba3f0823d4fc5458afe58cd4ab30a
Anna <a.trofimova@altcentr.ru>
2022-02-15 13:00:20 +0500

    Initial commit

---
+++ src/main/java/ru/hogwarts/course3/school/Course3homework1Application.java
@@ -0,0 +1,13 @@
+package ru.hogwarts.course3.school;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Course3homework1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Course3homework1Application.class, args);
+    }
+
+}
---
+++ src/test/java/ru/hogwarts/course3/school/Course3homework1ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.hogwarts.course3.school;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Course3homework1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
