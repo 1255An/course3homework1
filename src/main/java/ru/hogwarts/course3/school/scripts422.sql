5db824a501e679e152c6110d41c6f04425bbef0b
Anna <a.trofimova@altcentr.ru>
2022-03-18 09:59:49 +0500

    Course 4 homework 3 for checking

--- src/main/java/ru/hogwarts/course3/school/scripts422.sql
+++ src/main/java/ru/hogwarts/course3/school/scripts422.sql
@@ -1,12 +1,3 @@
-CREATE TABLE persons
-(
-    Id          REAL,
-    Name        TEXT PRIMARY KEY,
-    Age         INTEGER,
-    Has_license BOOLEAN,
-    Car_id      INT REFERENCES cars (id)
-);
-
 CREATE TABLE cars
 (
     Id    SERIAL,
@@ -14,6 +5,15 @@ CREATE TABLE cars
     MODEL VARCHAR,
     Price MONEY,
     CONSTRAINT car_id_pk PRIMARY KEY (Id)
+);
+
+CREATE TABLE persons
+(
+    Id          REAL,
+    Name        TEXT PRIMARY KEY,
+    Age         INTEGER,
+    Has_license BOOLEAN,
+    Car_id      INT REFERENCES cars (id)
 );
 
 SELECT persons.name, persons.age, persons.Has_license, cars.brand, cars.model
